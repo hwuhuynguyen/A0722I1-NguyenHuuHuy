5b1ead2aea17136f923fe2634bac9f1e7a1d83b5
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2022-08-19 11:34:27 +0700

    Submit module 1 assignments (Practice Conditional Structure)

--- Module 1/ConditionalStructure_2/Exercise/DoYouLoveMe/DoYouLoveMe.js
+++ Module 1/ConditionalStructure_2/Exercise/DoYouLoveMe/DoYouLoveMe.js
@@ -1,11 +1,12 @@
-let buttonNo = document.getElementById('no');
+// let buttonNo = document.getElementById('no');
+
 function clickYes() {
     alert('<3');
     console.log(window.innerHeight + " " + window.innerWidth);
 }
 function clickNo() {
-    document.getElementById('no').style.left = (getRandomArbitrary(0, window.innerWidth)) + 'px';
     document.getElementById('no').style.top = (getRandomArbitrary(0, window.innerHeight)) + "px";
+    document.getElementById('no').style.left = (getRandomArbitrary(0, window.innerWidth)) + 'px';
 
     console.log(document.getElementById('no').style.top + " " + document.getElementById('no').style.left);
 }
