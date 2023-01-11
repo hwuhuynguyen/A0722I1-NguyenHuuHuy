7af8b8a2092b9b84d5a20f1edc126519c05ac160
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2023-01-11 15:16:22 +0700

    Submit module 4 assignments (Views and Thymeleaf - Practice)

---
+++ Module 4/_03_Views_Thymeleaf/Practice/customer-management-thymeleaf/src/main/java/customer/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package customer.service;
+
+import customer.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
