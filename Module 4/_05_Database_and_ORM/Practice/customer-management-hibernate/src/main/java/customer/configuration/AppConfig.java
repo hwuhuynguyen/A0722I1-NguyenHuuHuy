3d21fc0821b725761aae24bb27ec82c739c740b9
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2023-02-06 20:06:48 +0700

    Submit module 4 assignments (Database and ORM - Practice)

---
+++ Module 4/_05_Database_and_ORM/Practice/customer-management-hibernate/src/main/java/customer/configuration/AppConfig.java
@@ -0,0 +1,70 @@
+package customer.configuration;
+
+import customer.service.CustomerService;
+import customer.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("customer")
+public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+    private ApplicationContext appContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appContext = applicationContext;
+    }
+
+/*
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3308/orm?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+        return dataSource;
+    }
+*/
+
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(appContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+}
---
+++ Module 4/_05_Database_and_ORM/Practice/customer-management-hibernate/src/main/java/customer/service/impl/HibernateCustomerServiceImpl.java
@@ -0,0 +1,107 @@
+package customer.service.impl;
+
+import customer.model.Customer;
+import customer.service.CustomerService;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class HibernateCustomerServiceImpl implements CustomerService {
+    private static EntityManager entityManager;
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.conf.xml")
+                    .buildSessionFactory();
+            entityManager = sessionFactory.createEntityManager();
+//            sessionFactory.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        String queryStr = "SELECT c FROM Customer AS c";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+        String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+
+            Customer origin = findOne(customer.getId());
+            origin.setName(customer.getName());
+            origin.setEmail(customer.getEmail());
+            origin.setAddress(customer.getAddress());
+
+            session.saveOrUpdate(origin);
+            transaction.commit();
+            return origin;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+
+    @Override
+    public void delete(Customer customer) {
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+}
