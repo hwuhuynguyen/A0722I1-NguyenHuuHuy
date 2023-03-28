7861a4cdbb5e50de0c10548d24713abecd7e2ddc
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2023-03-28 08:46:21 +0700

    Submit module 4 assignments (jQuery and AJAX - Practice)

---
+++ Module 4/_12_jQuery_and_AJAX/Practice/smartphone-management-jquery/src/main/java/com/repository/ISmartphoneRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import com.model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
---
+++ Module 4/_12_jQuery_and_AJAX/Practice/smartphone-management-jquery/src/main/java/com/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import com.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
