c4c9f7ad8cdeaa3497a34933e222715bb125aed2
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2023-04-01 14:50:32 +0700

    Submit module 4 assignments (Web security - Practice)

---
+++ Module 4/_14_Web_Security/Practice/spring-security/src/main/java/com/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.controller;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/home")
+    public ModelAndView index() {
+        return new ModelAndView("/index");
+    }
+
+    @GetMapping("/user")
+    public ModelAndView user(Principal principal) {
+        // Get authenticated user name from Principal
+        System.out.println(principal.getName());
+        return new ModelAndView("/user");
+    }
+
+    @GetMapping("/admin")
+    public ModelAndView admin() {
+        // Get authenticated username from SecurityContext
+        SecurityContext context = SecurityContextHolder.getContext();
+        System.out.println(context.getAuthentication().getName());
+        return new ModelAndView("/admin");
+    }
+}
---
+++ Module 4/_14_Web_Security/Practice/spring-security/src/main/java/com/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
